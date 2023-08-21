e3e8333ab0f1110b098c75d33e674b086c7ab1b1
ryota-09 <n.ryo950911@gmail.com>
2023-08-22 07:57:54 +0900

    s3をオリジンとするcloudfrontの作成

--- cloudfront.tf
+++ cloudfront.tf
@@ -5,6 +5,7 @@ resource "aws_cloudfront_distribution" "cf" {
   comment         = "cache distribution"
   price_class     = "PriceClass_All"
 
+  # to ELB
   origin {
     domain_name = aws_route53_record.route53_record.fqdn
     origin_id   = aws_lb.alb.name
@@ -14,6 +15,16 @@ resource "aws_cloudfront_distribution" "cf" {
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
       http_port              = 80
       https_port             = 443
+    }
+  }
+
+  # to S3
+  origin {
+    domain_name = aws_s3_bucket.s3_static_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.s3_static_bucket.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cf_s3_origin_access_identity.cloudfront_access_identity_path
     }
   }
 
@@ -35,6 +46,27 @@ resource "aws_cloudfront_distribution" "cf" {
     max_ttl                = 0
   }
 
+  ordered_cache_behavior {
+    path_pattern     = "/public/"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.s3_static_bucket.id
+
+    forwarded_values {
+      query_string = false
+      headers      = []
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -48,6 +80,10 @@ resource "aws_cloudfront_distribution" "cf" {
     minimum_protocol_version = "TLSv1.2_2019"
     ssl_support_method       = "sni-only"
   }
+}
+
+resource "aws_cloudfront_origin_access_identity" "cf_s3_origin_access_identity" {
+  comment = "S3 static bucket access identity"
 }
 
 resource "aws_route53_record" "route53_cloudfront" {
